aa792464fe2f50660fa7371f429d1c8fbd6fff41
gilbert <gilbertmotlatso@gmail.com>
2024-05-27 20:33:02 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const screenDisplay = document.getElementById("screenDisplay");
 
+
 function addToScreen(input) {
     screenDisplay.value += input;
 }
 
 function clearScreen() {
     screenDisplay.value = "";
+}
+
+function calculatePercentage() {
+    let currentValue = parseFloat(screenDisplay.value);
+
+    screenDisplay.value = currentValue * 0.01;
+     
 }
 
 function Calculate() {
